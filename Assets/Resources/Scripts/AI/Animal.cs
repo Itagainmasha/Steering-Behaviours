5d01bfa97162441934a972eeea3a2d5dbf0d266e
Masha Pylypchuk <55059744+Itagainmasha@users.noreply.github.com>
2021-12-06 17:57:02 +0200

    general animal, animal flair
    todo animal types - grouped, idle, enemy

---
+++ Assets/Resources/Scripts/AI/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Animal : MonoBehaviour
+{
+    [Header("Components")]
+    [Tooltip("Игровой менеджер, здесь хранится всё важное, он всего 1 на сцене")]
+    [SerializeField] internal GameManager gameManager;]
+
+    [Header("Animal: Options")]
+    [Tooltip("Здоровье животного")]
+    [SerializeField] internal int health = 1; 
+    [Tooltip("Является ли животное главным")]
+    [SerializeField] internal bool _mainAnimal = true;
+
+   
+    internal void GetDamage(int damage)
+    {
+        health -= damage; 
+        if (health == 0) 
+            Die(); 
+    }
+
+   
+    internal void Die()
+    {
+        gameManager.alives.Remove(transform); 
+        if (_mainAnimal) 
+            SetNewMainAnimal(); 
+        
+        Destroy(gameObject); 
+    }
+
+    private void SetNewMainAnimal()
+    {
+        if (GetComponent<GroupedAnimal>() != null) 
+        {
+            GroupedAnimal thisGroupedAnimal = GetComponent<GroupedAnimal>(); 
+            List<GroupedAnimal> _groupedAnimals = new List<GroupedAnimal>(); 
+            for (int i = 0; i < gameManager.groupedAnimals.Count; i++) 
+            {
+                GroupedAnimal groupedAnimal = gameManager.groupedAnimals[i]; 
+                if (groupedAnimal.groupID == thisGroupedAnimal.groupID && gameObject != groupedAnimal.gameObject) 
+                    _groupedAnimals[i] = groupedAnimal; 
+            }
+            int rand = Random.Range(0, _groupedAnimals.Count - 1); 
+            _groupedAnimals[rand]._mainAnimal = true; 
+        }
+    }
+
+    
+    protected virtual void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Bullet")) 
+            GetDamage(col.GetComponent<Bullet>().damage); 
+    }
+}
---
+++ Assets/Resources/Scripts/AI/Flair.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+
+/// Чутье животных
+
+public class Flair : Animal
+{
+    [Header("Flair: Options")]
+    [Tooltip("Скорость передвижения животного")]
+    [SerializeField] protected float _speed = 1f; 
+    [Tooltip("Радиус поиска добычи")]
+    [SerializeField] protected float _radiusSearchPrey = 2f; 
+    #region Conditions
+    [Tooltip("Ведётся ли преследование (true - да, false - нет)")]
+    [SerializeField] protected bool _chased = false; // Ведётся ли преследование (true - да, false - нет)
+    [Tooltip("Состояние животного (true - патрулирует, false - занят чем-то другим)")]
+    [SerializeField] protected bool _patrol = true; // Состояние животного (true - патрулирует, false - занят чем-то другим)
+    [Tooltip("Состояние животного (true - бегство, false - занят чем-то другим)")]
+    [SerializeField] protected bool _running = false; // Состояние животного (true - бегство, false - занят чем-то другим)
+    #endregion
+    [Tooltip("Опция, является ли животное вражеским")]
+    [SerializeField] private bool _enemy = false; 
+
+    protected Transform _target; 
+    protected float _timeReset = 0f; 
+
+    
+    protected void CalculateOptimalTarget()
+    {
+        float minDistance = 0f; 
+        foreach (Transform alive in gameManager.alives) 
+        {
+            if (alive != null && alive.gameObject.name == gameObject.name) continue; 
+
+            if (_enemy && _target == null) 
+                _timeReset = Time.time; 
+            float dist = Vector2.Distance(transform.position, alive.position); 
+            if (dist <= _radiusSearchPrey) 
+            {
+                if (minDistance == 0f) 
+                {
+                    minDistance = dist; 
+                    if (_enemy) 
+                        _timeReset = Time.time; 
+                }
+
+                if (dist <= minDistance) 
+                {
+                    minDistance = dist; 
+                    _target = alive; 
+                    if (_enemy) 
+                        Chase(true);  
+                    else
+                    {
+                        if ((gameObject.name == "GroupedAnimal" && _target.name == "IdleAnimal") || (gameObject.name == "GroupedAnimal" && _target.name != "EnemyAnimal")) continue;
+                        Running(true); 
+                    }  
+                }
+            }
+        }
+    }
+
+=
+    /// <param name="chase"></param>
+    protected void Chase(bool chase)
+    {
+        if (!chase) 
+            _target = null; 
+        _patrol = !chase; 
+        _chased = chase;
+    }
+
+
+    /// <param name="running"></param>
+    protected void Running(bool running)
+    {
+        if (!running) 
+            _target = null; 
+        _patrol = !running;
+        _running = running; 
+    }
+}
